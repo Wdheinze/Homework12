a76dc3aab893a3773070327571d15769a75d2b86
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-04-08 14:35:02 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,37 @@
 var config = {
     width: 600,
     height: 800,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     backgroundColor: 0x000000,
     scene: [Scene1, Scene2]
+
 }
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
 window.onload = function () {
     var game = new Phaser.Game(config);
+}
+
+//Code
+
+
+
+function preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
