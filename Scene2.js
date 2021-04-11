c2bf4caf682b02fd090069562c329e3a0996af3f
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-04-11 17:53:24 -0600

    Update Scene2.js

--- Scene2.js
+++ Scene2.js
@@ -86,6 +86,7 @@ class Scene2 extends Phaser.Scene {
 
     // don't need the keyword function in front of update
     update() {
+        
         if (gameOver) {
             return;
         }
@@ -111,50 +112,53 @@ class Scene2 extends Phaser.Scene {
         }
     }
 
-    // don't need the keyword function in front of collectStar
-    collectStar(player, star) {
-        star.disableBody(true, true);
-        score += 1;
-        scoreText.setText('Score: ' + score);
-
-        if (stars.countActive(true) === 0) {
-            //  A new batch of stars to collect
-            stars.children.iterate(function (child) {
-
-                child.enableBody(true, child.x, 0, true, true);
-
-            });
-
-            var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-
-            var bomb = bombs.create(x, 16, 'bomb');
-            bomb.setBounce(1);
-            bomb.setCollideWorldBounds(true);
-            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-            bomb.allowGravity = false;
-
-        }
-    }
-    // don't need the function keyword in front of hitSpikes
-    hitSpikes(player, spikes) {
-        this.physics.pause();
-
-        player.setTint(0xff0000);
-
-        player.anims.play('turn');
-
-        gameOver = true;
-    }
-    // don't need the keyword function in front of hitBomb
-    hitBomb(player, bomb) {
-        this.physics.pause();
-
-        player.setTint(0xff0000);
-
-        player.anims.play('turn');
-
-        gameOver = true;
-    }
-    // } // don't need this curly brace
+   
 }
+
+ // don't need the keyword function in front of collectStar
+ function collectStar(player, star) {
+    console.log("HI");
+    star.disableBody(true, true);
+    score += 1;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0) {
+        //  A new batch of stars to collect
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    }
+}
+// don't need the function keyword in front of hitSpikes
+function hitSpikes(player, spikes) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
+// don't need the keyword function in front of hitBomb
+function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
+// } // don't need this curly brace
